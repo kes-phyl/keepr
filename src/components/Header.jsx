4d9aea00a8d6feb9d67950b5411be9eded472e9e
kes-phyl <danphylbert@gmail.com>
2022-11-15 04:23:14 +0000

    initial commit plus project setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+//header function 
+
+function Header(){
+    return <header>
+          <h1>Keeper App</h1>
+    </header>
+      
+    
+}
+
+export default Header;
---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+function Note(){
+    return <div class='note'>
+        <h1>Title of Note</h1>
+        <p>Paragraph containing the note which would ultimately be a bunch of lorem ipsum. </p>
+    </div>
+}
+
+
+export default Note;
